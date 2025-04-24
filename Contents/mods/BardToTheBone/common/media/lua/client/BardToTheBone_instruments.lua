b06ac993a21f565ae481489cdd3c81208266631a
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-04-23 21:01:19 -0400

    Update BardToTheBone_instruments.lua

--- Contents/mods/BardToTheBone/common/media/lua/client/BardToTheBone_instruments.lua
+++ Contents/mods/BardToTheBone/common/media/lua/client/BardToTheBone_instruments.lua
@@ -1,14 +1,25 @@
+--- NEED ITEM TO BE MADE:
+--- Accordion
+--- Bikehorn
+--- GuitarElectricBass
+--- Glockenspiel
+--- Trombone
+--- Xylophone
+
+--- [SOLID]
 -- Banjo
--- GuitarAcoustic
 -- GuitarElectric
--- GuitarElectricBass
--- Flute
--- Saxophone
--- Trumpet
--- Violin
--- Keytar
+-- GuitarAcoustic
 -- Harmonica
--- Whistle
--- Whistle_Bone
--- TuningFork
+-- Piano [ON MAP ONLY] ?
+-- Flute [Recorder] ?
+-- Saxophone
+-- Violin
+
+--- [NEED] Sounds For
+-- Trumpet [NEED]
+-- Keytar [NEED]
+-- Whistle [NEED] ?
+-- Whistle_Bone [NEED] ?
+-- TuningFork [NEED] ?
 
